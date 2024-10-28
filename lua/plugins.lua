a9f612274324e5e0a605fb88aec301775566666a
outsomneack <meniere.56@gmail.com>
2024-10-28 18:36:17 +0530

    initializing basic directory structure, options, and installing lazy

---
+++ lua/plugins.lua
@@ -0,0 +1,23 @@
+-- Sets up lazy.nvim
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", 
+	  "clone", 
+	  "--filter=blob:none", 
+	  "--branch=stable", 
+	  lazyrepo, 
+	  lazypath 
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{
+		"rebelot/kanagawa.nvim",
+		config = function()
+			vim.cmd.colorscheme("kanagawa-wave")
+		end,
+	},
+})
